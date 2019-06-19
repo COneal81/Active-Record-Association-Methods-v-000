c27fc598735372d967a56f8d6f09c98fb3c2d704
Kari O'Neal <triadfirewood@gmail.com>
2019-06-19 18:31:05 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,8 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists.name
+    artist = artists.collect do |n|
+      
      binding.pry
     # return an array of strings containing every musician's name
   end
